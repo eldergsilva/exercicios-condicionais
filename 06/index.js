81fbdc6c433ff3c65e2d1133c602295de7048eb4
eldergsilva <eldergsilva@gmail.com>
2026-03-05 18:56:39 -0300

    exercicio 07 definindo as posições

---
+++ 06/index.js
@@ -0,0 +1,17 @@
+const alturaEmCm = 205;
+
+if (alturaEmCm < 180) {
+    console.log("REPROVADO");
+
+} else if (alturaEmCm <= 185) {
+    console.log("LÍBERO");
+
+} else if (alturaEmCm <= 195) {
+    console.log("PONTEIRO");
+
+} else if (alturaEmCm <= 205) {
+    console.log("OPOSTO");
+
+} else {
+    console.log("CENTRAL");
+}
