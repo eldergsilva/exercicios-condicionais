c80a87b9e1bb95a94a6b973b117c21159138c3d2
eldergsilva <eldergsilva@gmail.com>
2026-03-05 20:40:42 -0300

    exercicio 09 transformar nota em conceito

---
+++ 09/index.js
@@ -0,0 +1,20 @@
+const nota = 7;
+
+if (nota < 4) {
+    console.log("O estudante obteve conceito E");
+
+} else if (nota < 6) {
+    console.log("O estudante obteve conceito D");
+
+} else if (nota < 8) {
+    console.log("O estudante obteve conceito C");
+
+} else if (nota < 9) {
+    console.log("O estudante obteve conceito B");
+
+} else if (nota <= 10) {
+    console.log("O estudante obteve conceito A");
+
+} else {
+    console.log("Nota inválida");
+}
