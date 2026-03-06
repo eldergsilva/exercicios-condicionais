08a3a372f2c3d5e13a52c28fa811878c4579d938
eldergsilva <eldergsilva@gmail.com>
2026-03-06 16:50:25 -0300

    exercicio 15 Nome para Exibição

---
+++ 15/index.js
@@ -0,0 +1,13 @@
+const primeiroNome = "Mario";
+const sobrenome = "Bros";
+const apelido = "";
+if (apelido) {
+    console.log(apelido);
+    
+}else if(sobrenome){
+    console.log(`${primeiroNome} ${sobrenome}`);
+    
+}else{
+    console.log(primeiroNome);
+    
+}
