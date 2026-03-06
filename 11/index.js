9e4eec7dd04ab630e2a6be7e1b3b9d1ca371b4e6
eldergsilva <eldergsilva@gmail.com>
2026-03-05 21:34:00 -0300

    exercicio 11 parcela do Curso Cubos Academy

---
+++ 11/index.js
@@ -0,0 +1,21 @@
+const rendaMensalEmCentavos = 190000;
+const mesesDecorridos = 61;
+const totalJaPagoPeloAluno = 1000000;
+
+let valorDaParcela;
+
+if (mesesDecorridos > 60 || totalJaPagoPeloAluno >= 1800000) {
+
+    console.log("O valor da parcela desse mês é R$ 0 reais. O aluno não deve mais nada.");
+
+} else if (rendaMensalEmCentavos >= 200000) {
+
+    valorDaParcela = rendaMensalEmCentavos * 0.18;
+
+    console.log(`O valor da parcela desse mês é R$ ${(valorDaParcela / 100).toFixed(2)} reais`);
+
+} else {
+
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.");
+
+}
