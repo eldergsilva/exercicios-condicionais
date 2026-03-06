394b8faf64386db36b55969a0f5dbe57ff9dcf70
eldergsilva <eldergsilva@gmail.com>
2026-03-06 17:01:47 -0300

    exercicio 16 Rematrícula Escolar

---
+++ 16/index.js
@@ -0,0 +1,6 @@
+const idadeDoAluno = 16;
+const possuiResponsavel = false;
+
+(idadeDoAluno >=18 || possuiResponsavel)
+ ? console.log("Rematrícula realizada com sucesso")
+ : console.log("Não é possível fazer a rematrícula");
